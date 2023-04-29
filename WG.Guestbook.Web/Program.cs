6ce502941523cef18b7285d4921b2fd37a1f6195
johan <johan@PC-MARTINA>
2023-04-29 20:29:07 +0200

    Add project files.

---
+++ WG.Guestbook.Web/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
