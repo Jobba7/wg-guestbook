ded544f377787700118ec5fed5ecaa9013d9fce5
Johannes <johannes.pforr@outlook.de>
2023-04-30 12:56:18 +0200

    Add registration

---
+++ WG.Guestbook.Web/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WG.Guestbook.Web.Domain;
+using WG.Guestbook.Web.Models.Account;
+
+namespace WG.Guestbook.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model, string? returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User(model.UserName);
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            _logger.LogInformation($"Registration for user {user.UserName}: {result}");
+
+            if (!result.Succeeded)
+            {
+                var justOneError = result.Errors.Count() == 1;
+                foreach (var error in result.Errors)
+                {
+                    if (error.Code.Contains("user", StringComparison.OrdinalIgnoreCase) && justOneError)
+                    {
+                        ModelState.AddModelError(nameof(model.UserName), error.Description);
+                    }
+                    else if (error.Code.Contains("password", StringComparison.OrdinalIgnoreCase) && justOneError)
+                    {
+                        ModelState.AddModelError(nameof(model.Password), error.Description);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+                return View(model);
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: true);
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WG.Guestbook.Web/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WG.Guestbook.Web.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Nutzername")]
+        [Required(ErrorMessage = "Bitte gib einen Nutzernamen ein.")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Display(Name = "Passwort")]
+        [Required(ErrorMessage = "Bitte gib ein Passwort ein.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Bestätige das Passwort")]
+        [Required(ErrorMessage = "Bitte bestätige dein Passwort.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwörter stimmen nicht überein.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
